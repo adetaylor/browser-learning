114b672d39e08a5192c74d444755d12f8e4b8bea
Adrian Taylor <adetaylor@chromium.org>
2025-07-19 12:13:38 +0100

    Update install-prerequisites.py

--- src/install-prerequisites.py
+++ src/install-prerequisites.py
@@ -5,6 +5,7 @@ import pip
 
 script_dir = os.path.dirname(os.path.realpath(__file__))
 requirements_file = os.path.join(script_dir, "requirements.txt")
-pip.main(["install", "--user", "-r", requirements_file])
-requirements_file = os.path.join(script_dir, "requirements-win.txt")
-pip.main(["install", "--user", "-r", requirements_file])
+requirements_win_file = os.path.join(script_dir, "requirements-win.txt")
+files = [requirements_file, requirements_win_file]:
+for f in files:
+  pip.main(["install", "--user", "-r", f, "--trusted-host", "pypi.python.org", "--trusted-host", "pypi.org", "--trusted-host", "files.pythonhosted.org”])
